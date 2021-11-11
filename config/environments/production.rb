5a2be6eba76be0dc24032e4c475bc72874410727
hamza <hamnaveed99@gmail.com>
2021-11-11 15:49:32 +0500

    feat(asset-integration): recover password email authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,18 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+ 
+  config.assets.raise_runtime_errors = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'http://natural-flavors.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+     :address              => "smtp.gmail.com",
+     :port                 =>  587,
+     :user_name            => 'omqa2794@gmail.com',
+     :password             => 'PakistaN@123',
+     :authentication       => "plain",
+   }
+
 end
