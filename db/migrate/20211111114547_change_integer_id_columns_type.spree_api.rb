94aac79c23fb66d64fb79e99a4946d2c24efa6ca
hamza <hamnaveed99@gmail.com>
2021-11-12 16:29:40 +0500

    feat(asset-integration):migration fix

--- db/migrate/20211111114547_change_integer_id_columns_type.spree_api.rb
+++
@@ -1,9 +0,0 @@
-# This migration comes from spree_api (originally 20210727102516)
-  def change
-    change_column :spree_oauth_access_grants, :resource_owner_id, :bigint
-    change_column :spree_oauth_access_grants, :application_id, :bigint
-
-    change_column :spree_oauth_access_tokens, :resource_owner_id, :bigint
-    change_column :spree_oauth_access_tokens, :application_id, :bigint
-  end
-end
