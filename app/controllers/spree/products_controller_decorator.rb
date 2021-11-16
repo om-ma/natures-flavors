c7e9b76644f781196c9e0a9f52020373f46ff414
umairrazam <omairrr.azam@gmail.com>
2021-11-16 12:08:30 +0500

    fix(text): indentation

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -2,7 +2,7 @@ module Spree
 	module ProductsControllerDecorator
 		def index
 			redirect_to page_not_found_path
-	  end
+		end
 	end
 end
 ::Spree::ProductsController.prepend Spree::ProductsControllerDecorator
