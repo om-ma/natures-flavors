3784bf09cf4872a208e360ef25aec73e8cdcf3b3
hamza <hamnaveed99@gmail.com>
2021-11-23 21:31:19 +0500

    fix: bug fixing of sprint 1

--- app/models/user_decorator.rb
+++ app/models/user_decorator.rb
@@ -4,7 +4,7 @@ module Spree
         def self.prepended(base)
             base.validates :password,
               format: { with: /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d][^ ]{7,}\z/,
-                        message: 'must contain at least 7 characters, including at least 1 letter and 1 number.'
+                        message: 'including at least 1 letter and 1 number.'
                        }
           end
   
