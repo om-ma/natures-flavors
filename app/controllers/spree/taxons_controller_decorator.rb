12309ff2768b89e9374bed2505906bb0c3d8d555
omair azam <omairr.azam@gmail.com>
2021-12-17 14:32:12 +0500

    fix: Empty commit for heroku

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -23,6 +23,7 @@ Spree::TaxonsController.class_eval do
     @searcher = build_searcher(search_params)
     products_searcher = @searcher.retrieve_products.send(sorting_scope)
 
+    
     if params[:sorting] == "bestsellers"
       @products =  products_searcher.best_sellers
     else
