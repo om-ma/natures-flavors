82738ad82ff15fc9b5481a9ce96854f310b0943d
umairrazam <omairrr.azam@gmail.com>
2022-01-06 19:44:06 +0500

    fix

--- vendor/assets/javascripts/spree/frontend/saved_address.js
+++ vendor/assets/javascripts/spree/frontend/saved_address.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     $('.address-select-change-js').on('change', function() {
       if (this.value == "new_address_selected"){
         $("#order_bill_address_id_0").click();
+        //console.log(document.getElementById("billing"))
+        $('#billing').find('input:text').val('');
       }
       else{
           let selectedAddress_id = this.value
