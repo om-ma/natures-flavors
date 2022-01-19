e905ecea608300bb32a7097a5e572962c06eb2c9
umairrazam <omairrr.azam@gmail.com>
2022-01-19 21:08:57 +0500

    fix/account-page-wishlist

---
+++ app/controllers/spree/users_controller_decorator.rb
@@ -0,0 +1,10 @@
+module Spree 
+    module UsersControllerDecorator
+      def show
+        load_object
+        @favorite_products= spree_current_user.favorite_products
+      end
+    end
+  end
+  ::Spree::UsersController.prepend Spree::UsersControllerDecorator if ::Spree::UsersController.included_modules.exclude?(Spree::UsersControllerDecorator) 
+  
