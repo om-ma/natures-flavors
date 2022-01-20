d15da24d3a3f392e360d3a50c19edbae7dec879c
umairrazam <omairrr.azam@gmail.com>
2022-01-20 22:11:16 +0500

    fix(js-issue)

--- vendor/assets/javascripts/spree/frontend/deals_expiry.js
+++ vendor/assets/javascripts/spree/frontend/deals_expiry.js
@@ -1,20 +1,21 @@
-$(document).on('ready page:load', function () {
+document.addEventListener('turbolinks:before-render', () => {
   var SalesExpiryDate= $('#deals-expiry').attr("data-SalesExpiryDate");
-  var countDownDate = new Date(SalesExpiryDate).getTime();
-  var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      
-    $('#deals-expiry').html("<strong>Deal Expiers in: <span>" + days + "</strong> day </span><span><strong>" + hours + "</strong> hours </span><strong><span>"
-    + seconds + "</strong> seconds </span> ")
-       
-    if (distance < 0) {
-      clearInterval(x);
-      $('#deals-expiry').html("Expired ")
-    }
-  }, 1000);
+    var countDownDate = new Date(SalesExpiryDate).getTime();
+    var x = setInterval(function() {
+    
+
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+      $('#deals-expiry').html("<strong>Deal Expiers in: <span>" + days + "</strong> day </span><span><strong>" + hours + "</strong> hours </span><strong><span>"
+      + seconds + "</strong> seconds </span> ")
+        
+      if (distance < 0) {
+        clearInterval(x);
+        $('#deals-expiry').html("Expired ")
+      }
+    }, 1000);
 });
