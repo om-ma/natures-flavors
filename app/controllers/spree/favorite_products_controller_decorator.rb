a6ddcd0061c756fa84dfeeabeef3dbb83935041b
umairrazam <omairrr.azam@gmail.com>
2022-01-20 16:48:33 +0500

    fix

--- app/controllers/spree/favorite_products_controller_decorator.rb
+++ app/controllers/spree/favorite_products_controller_decorator.rb
@@ -6,6 +6,13 @@ module Spree
     def set_favourite_product
       @favourite_product = Spree::Product.find_by_id params[:id]
     end
+    def destroy
+      @success = @favorite.destroy
+      respond_to do |format|
+        format.html { redirect_back_or_default(account_path) }
+        format.js
+      end
+    end
   end
 end
 ::Spree::FavoriteProductsController.prepend Spree::FavoriteProductsControllerDecorator
--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -2,7 +2,8 @@ module Spree
     module UsersControllerDecorator
       def show
         load_object
-        @favorite_products= spree_current_user.favorite_products
+        favorite_products = spree_current_user.favorite_products
+        @favorite_products= favorite_products.present? ? favorite_products.last(3) : []
       end
     end
   end
