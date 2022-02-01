d1f4f3e97c18fd6807be4932baea897e902ae61f
umairrazam <omairrr.azam@gmail.com>
2022-02-01 21:36:28 +0500

    fix(extra-methods): home controller

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -69,27 +69,4 @@ def sale
     end
   end
 
-  private
-
-  def etag_index
-    [
-      store_etag,
-      last_modified_index,
-    ]
-  end
-
-  def last_modified_index
-    page_last_modified = @cms_home_page&.maximum(:updated_at)&.utc if @cms_home_page.respond_to?(:maximum)
-    current_store_last_modified = current_store.updated_at.utc
-
-    [page_last_modified, current_store_last_modified].compact.max
-  end
-
-  def accurate_title
-    @cms_home_page&.seo_title || super
-  end
-
-  def load_homepage
-    @cms_home_page = current_store.homepage(I18n.locale)
-  end
 end
