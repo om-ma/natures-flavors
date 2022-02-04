be404b758887623ef2697efda7b13904298cca78
umairrazam <omairrr.azam@gmail.com>
2022-02-04 16:52:26 +0500

    fix

--- vendor/assets/javascripts/spree/frontend/taxon_filter.js
+++ vendor/assets/javascripts/spree/frontend/taxon_filter.js
@@ -1,15 +1,16 @@
-$( document ).ready(function() {
+$( document ).on('turbolinks:load', function() {
+
+  $(".product-sort-js").on('click', function(e){
+    var click_filter = $(this).data("filter-type");
+    $("." + click_filter)[0].click();
+  });
+
   $('.sub-child-manu-js').click(function (e) {
      var child_link = $(this).find('a').attr('href');
      var url = window.location.origin;
      window.location.href = url + child_link;
 
     });
- $(".product-sort-js").click(function(){
-      var click_filter = $(this).data("filter-type");
-      $("." + click_filter)[0].click();
-    });
-
 
    $('.mobile-category-toggle').on('click', function(e) {
     $('.mobile-category-toggle').toggleClass("clicked");
