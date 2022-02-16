c4b872c2a5d6013fcfcc6cfaeca78cf700bf48cf
umairrazam <omairrr.azam@gmail.com>
2022-02-16 14:20:06 +0500

    fix(pr-issues)

--- app/controllers/spree/add_to_cart_controller.rb
+++ app/controllers/spree/add_to_cart_controller.rb
@@ -3,7 +3,7 @@ module Spree
     def create
       variant = Spree::Product.find_by(id:params[:id]).master
       order = current_order || Order.incomplete.find_by(token: cookies.signed[:token]) || current_order(create_order_if_necessary: true)
-      line_item = Spree::Dependencies.cart_add_item_service.constantize.call(order: order, variant: variant,quantity: 1, options: params).value
+      line_item = Spree::Dependencies.cart_add_item_service.constantize.call(order: order, variant: variant,quantity: 1, options: {}).value
       if line_item.save
         flash[:success] = "#{variant.name} successfully added to cart"
       else
--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -1,5 +1,4 @@
 Spree::TaxonsController.class_eval do
-
   def show
     if !http_cache_enabled? || stale?(etag: etag, last_modified: last_modified, public: true)
       load_products
@@ -22,25 +21,14 @@ Spree::TaxonsController.class_eval do
 
     @searcher = build_searcher(search_params)
     products_searcher = @searcher.retrieve_products.send(sorting_scope)
-
     if params[:sorting] == "bestsellers"
       @products =  products_searcher.best_sellers
     else
       @products =   products_searcher
       @taxon = Spree::Taxon.friendly.find(params[:id])
       return unless @taxon
-      @searcher = build_searcher(params.merge(taxon: @taxon, include_images: true))
-      list      = ['descend_by_popularity', 'ascend_by_name', 'descend_by_name']
-
-      if list.include?(params[:sorting])
-        sorting_scope = params[:sorting].try(:to_sym) || :ascend_by_name
-      else
-        sorting_scope = :ascend_by_name
-      end
-
-      @products   = @searcher.retrieve_products.send(sorting_scope).page(params[:page]).per(params['per_page'])
-      @taxonomies = Spree::Taxonomy.includes(root: :children).where('name ILIKE ?','%PRODUCTS%')
     end
+    @products   = @products.page(params[:page]).per(params['per_page'])
+    @taxonomies = Spree::Taxonomy.includes(root: :children).where('name ILIKE ?','%PRODUCTS%')
   end
-
 end
