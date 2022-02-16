c38dd67e846ab458966640e4bf928b6354a59227
umairrazam <omairrr.azam@gmail.com>
2022-02-16 15:46:38 +0500

    fix(sorting issue on catagory page)

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -11,24 +11,13 @@ Spree::TaxonsController.class_eval do
       taxon: @taxon,
       include_images: true
     )
-
-    list = ['descend_by_popularity', 'ascend_by_name', 'descend_by_name']
-    if list.include?(params[:sorting])
-      sorting_scope = params[:sorting].try(:to_sym) || :ascend_by_name
-    else
-      sorting_scope = :ascend_by_name
-    end
-
     @searcher = build_searcher(search_params)
-    products_searcher = @searcher.retrieve_products.send(sorting_scope)
+    products_searcher = @searcher.retrieve_products
+
     if params[:sorting] == "bestsellers"
       @products =  products_searcher.best_sellers
     else
       @products =   products_searcher
-      @taxon = Spree::Taxon.friendly.find(params[:id])
-      return unless @taxon
     end
-    @products   = @products.page(params[:page]).per(params['per_page'])
-    @taxonomies = Spree::Taxonomy.includes(root: :children).where('name ILIKE ?','%PRODUCTS%')
   end
 end
