c17e4926968a03b2beaa7a85d08c310600d8a9c7
Duc T. Lam <ducl_13@yahoo.com>
2022-03-30 13:58:19 -0700

    Timezone set to PST

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,7 @@ module NaturesFlavors
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = "Pacific Time (US & Canada)"
   end
 end
