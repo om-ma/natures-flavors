c5ae9938f703e0f6356265eaf8ad679fb099f676
umairrazam <omairrr.azam@gmail.com>
2022-04-01 21:51:10 +0500

    fix(issues): sorting

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -6,15 +6,21 @@ Spree::TaxonsController.class_eval do
   end
 
   def load_products
+
     search_params = params.merge(
       current_store: current_store,
       taxon: @taxon,
       include_images: true
     )
+    
+    if params[:sort_by] == "bestsellers"
+      search_params.delete :sort_by
+    end
+
     @searcher = build_searcher(search_params)
     products_searcher = @searcher.retrieve_products
 
-    if params[:sorting] == "bestsellers"
+    if params[:sort_by] == "bestsellers"
       @products =  products_searcher.best_sellers
     else
       @products =   products_searcher
