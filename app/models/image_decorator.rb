903675a2afac01d73dcb90de79d5f68871e2ada4
Duc T. Lam <ducl_13@yahoo.com>
2022-04-05 10:10:37 -0700

    Add image styles

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -7,7 +7,15 @@ Spree::Image.class_eval do
       mini: '48x48>',
       small: '180x180>',
       product: '240x240>',
-      large: '1000x1000>', # product page images
+      pdp_thumbnail: '180x180>',
+      plp_and_carousel: '448x600>',
+      plp_and_carousel_xs: '180x180>',
+      plp_and_carousel_sm: '180x180>',
+      plp_and_carousel_md: '180x180>',
+      plp_and_carousel_lg: '240x240>',
+      large: '1000x1000>',
+      plp: '240x240>',
+      zoomed: '1000x1000>'
     }
   end
 
