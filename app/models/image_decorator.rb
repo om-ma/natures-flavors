f5ff563c3f1ed28c8feb5971cd64c9ba54c4e279
Duc T. Lam <ducl_13@yahoo.com>
2022-04-08 14:20:28 -0700

    Refactor

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -1,37 +1,42 @@
-Spree::Image.class_eval do
+module Spree
+  module ImageDecorator
 
-  #after_save :create_sizes
-
-  def self.styles
-    {
-      mini: '48x48>',
-      small: '180x180>',
-      product: '240x240>',
-      pdp_thumbnail: '180x180>',
-      plp_and_carousel: '448x600>',
-      plp_and_carousel_xs: '180x180>',
-      plp_and_carousel_sm: '180x180>',
-      plp_and_carousel_md: '180x180>',
-      plp_and_carousel_lg: '240x240>',
-      large: '1000x1000>',
-      plp: '240x240>',
-      zoomed: '1000x1000>'
-    }
-  end
-
-  def create_sizes
-    Spree::Image.styles.keys.each do |style|
-      obj = self.url(style)
-      obj.processed
+    def self.prepended(base)
+      #after_save :create_sizes
     end
-  end
 
-  def my_cf_image_url(style)
-    default_options = Rails.application.default_url_options
-    ActiveStorage::Current.host = default_options[:host]
-    str = self.url(style).service_url
-    path = str.split('//').last.split("/",2).last
-    Rails.env.development? ? str : "https://#{ENV['CLOUDFRONT_ASSET_URL']}/#{path}"
-  end
+    def self.styles
+      {
+        mini: '48x48>',
+        small: '180x180>',
+        product: '240x240>',
+        pdp_thumbnail: '180x180>',
+        plp_and_carousel: '448x600>',
+        plp_and_carousel_xs: '180x180>',
+        plp_and_carousel_sm: '180x180>',
+        plp_and_carousel_md: '180x180>',
+        plp_and_carousel_lg: '240x240>',
+        large: '1000x1000>',
+        plp: '240x240>',
+        zoomed: '1000x1000>'
+      }
+    end
 
+    def create_sizes
+      Spree::Image.styles.keys.each do |style|
+        obj = self.url(style)
+        obj.processed
+      end
+    end
+
+    def my_cf_image_url(style)
+      default_options = Rails.application.default_url_options
+      ActiveStorage::Current.host = default_options[:host]
+      str = self.url(style).service_url
+      path = str.split('//').last.split("/",2).last
+      Rails.env.development? ? str : "https://#{ENV['CLOUDFRONT_ASSET_URL']}/#{path}"
+    end
+
+  end
 end
+::Spree::Image.prepend(Spree::ImageDecorator)
