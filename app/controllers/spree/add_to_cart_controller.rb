54714012a7ac973a0f8ff2ba2f577eabff30b610
umairrazam <omairrr.azam@gmail.com>
2022-04-17 00:17:54 +0500

    fix(issues): updated

--- app/controllers/spree/add_to_cart_controller.rb
+++ app/controllers/spree/add_to_cart_controller.rb
@@ -1,7 +1,6 @@
 module Spree
   class AddToCartController < Spree::StoreController
     def create
-
       variant = Spree::Product.find_by(id:params[:id]).master
       @order = current_order || Order.incomplete.find_by(token: cookies.signed[:token]) || current_order(create_order_if_necessary: true)
       line_item = Spree::Dependencies.cart_add_item_service.constantize.call(order: @order, variant: variant,quantity: 1, options: {}).value
