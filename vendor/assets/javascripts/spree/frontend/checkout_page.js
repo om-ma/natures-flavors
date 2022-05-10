06f68e0d0b30c8e48aad26fc6caa62caf1e5bcdc
umairrazam <omairrr.azam@gmail.com>
2022-05-10 13:33:40 +0500

    fix(checkout): mobile view

--- vendor/assets/javascripts/spree/frontend/checkout_page.js
+++ vendor/assets/javascripts/spree/frontend/checkout_page.js
@@ -2,6 +2,12 @@ $( document ).on('turbolinks:load', function() {
 	$(".design-submit-next-step").on('click',function(){
 		$("#checkout-submit-to-next-step").click();
 	});
+
+	$('.show-cart-summary').on('click', function(e) {
+	    $('.show-cart-summary').toggleClass("clicked");
+	    $('.mobile-cart-summary').toggleClass("show");
+	    e.preventDefault();
+	});
 	
 	$("#is_user_address_saved").on('change',function(){
 		if (this.checked){
