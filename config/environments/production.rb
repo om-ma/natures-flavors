f1e233fa14a65d28ef2edde93b3c3522cb862892
Duc T. Lam <ducl_13@yahoo.com>
2022-05-17 13:53:10 -0700

    Change config.assets.js_compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -92,12 +92,11 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.assets.raise_runtime_errors = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: "www.naturesflavors.com" }
-  config.action_mailer.asset_host = "https://www.naturesflavors.com"
+  config.action_mailer.asset_host = "https://#{ENV['CLOUDFRONT_ASSET_URL']}"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "email-smtp.us-east-1.amazonaws.com",
