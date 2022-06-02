e17afd3b0ae62685e22cdc3bc1e3a51a048d0115
umairrazam <omairrr.azam@gmail.com>
2022-06-02 17:53:54 +0500

    fix multiple otpion values

--- vendor/assets/javascripts/spree/frontend/views/spree/shared/variant_select.js
+++ vendor/assets/javascripts/spree/frontend/views/spree/shared/variant_select.js
@@ -87,7 +87,7 @@ this.updateStructuredData = function() {
 
 this.initializeColorVarianTooltip = function() {
   var colorVariants = $('.color-select-label[data-toggle="tooltip"]')
-  colorVariants.tooltip({
-    placement: 'bottom'
-  })
+  // colorVariants.tooltip({
+  //   placement: 'bottom'
+  // })
 }
