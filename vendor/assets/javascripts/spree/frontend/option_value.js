d9d189716c15e450069fee5ab90f1361781281a8
umairrazam <omairrr.azam@gmail.com>
2022-06-08 22:42:44 +0500

    fix single vairant issue

--- vendor/assets/javascripts/spree/frontend/option_value.js
+++ vendor/assets/javascripts/spree/frontend/option_value.js
@@ -10,4 +10,12 @@ $( document ).on('turbolinks:load', function() {
   });
   $(".option-values-dropdown").click()
   $('body').click()
+  if ($(".color-select-label")[0] != null ){
+    $(".color-select-label")[0].click()
+  }
+  let selected_option_value = $(".js_option_value_list input:radio:not(:disabled)")[0]
+  let selected_value_presentation = $(selected_option_value).data("presentation")
+  $(selected_option_value).click()
+  let option_type_id = "." + $(selected_option_value).data("option-type-id")
+  $(option_type_id).html(selected_value_presentation)
 });
