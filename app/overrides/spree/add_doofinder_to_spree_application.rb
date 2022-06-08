b86859f7868046c70c1461d71627dce982c82642
Duc T. Lam <ducl_13@yahoo.com>
2022-06-08 08:17:13 -0700

    Remove doofinder search

--- app/overrides/spree/add_doofinder_to_spree_application.rb
+++ app/overrides/spree/add_doofinder_to_spree_application.rb
@@ -1,6 +1,6 @@
-Deface::Override.new(
-  virtual_path: 'spree/layouts/spree_application',
-  name: 'add_doofinder_to_spree_application',
-  insert_bottom: "[data-hook='body']",
-  partial: 'spree/shared/doofinder'
-)
+#Deface::Override.new(
+#  virtual_path: 'spree/layouts/spree_application',
+#  name: 'add_doofinder_to_spree_application',
+#  insert_bottom: "[data-hook='body']",
+#  partial: 'spree/shared/doofinder'
+#)
