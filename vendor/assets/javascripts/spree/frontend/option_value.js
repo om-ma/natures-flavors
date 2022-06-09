495030ecdae187942f4b71e117c306fef0928930
umairrazam <omairrr.azam@gmail.com>
2022-06-09 21:56:06 +0500

    fix single variant issue

--- vendor/assets/javascripts/spree/frontend/option_value.js
+++ vendor/assets/javascripts/spree/frontend/option_value.js
@@ -3,19 +3,30 @@ $( document ).on('turbolinks:load', function() {
     let value = $(this).html()
     $(".js_color_value").html(value)
   });
+
   $(".js_select_option_value").click(function() {
     let value = $(this).html()
     let data_id = $(this).data("dropdown-value-id")
     $(".js_option_value_"+ data_id).html(value)
   });
+
+  $(".js-variant-list").click(function(){
+    let selected_variant_sku = $(this).data("varient-sku")
+    $("#js_selected_variantt_sku").html(selected_variant_sku)
+  })
+
   $(".option-values-dropdown").click()
   $('body').click()
+
   if ($(".color-select-label")[0] != null ){
     $(".color-select-label")[0].click()
   }
+
   let selected_option_value = $(".js_option_value_list input:radio:not(:disabled)")[0]
   let selected_value_presentation = $(selected_option_value).data("presentation")
+
   $(selected_option_value).click()
+
   let option_type_id = "." + $(selected_option_value).data("option-type-id")
   $(option_type_id).html(selected_value_presentation)
 });
