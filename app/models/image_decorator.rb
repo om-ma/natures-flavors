8e8a0fd70fde8efbf36fcf591f855601d6cb3f1a
Duc T. Lam <ducl_13@yahoo.com>
2022-06-21 19:23:48 -0700

    Refactor for after_save to work

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -1,42 +1,20 @@
-module Spree
-  module ImageDecorator
+Spree::Image.class_eval do
 
-    def self.prepended(base)
-      #after_save :create_sizes
+  after_save :create_sizes
+  
+  def create_sizes
+    Spree::Image.styles.keys.each do |style|
+      obj = self.url(style)
+      obj.processed
     end
-
-    def self.styles
-      {
-        mini: '48x48>',
-        small: '180x180>',
-        product: '240x240>',
-        pdp_thumbnail: '180x180>',
-        plp_and_carousel: '448x600>',
-        plp_and_carousel_xs: '180x180>',
-        plp_and_carousel_sm: '180x180>',
-        plp_and_carousel_md: '180x180>',
-        plp_and_carousel_lg: '240x240>',
-        large: '1000x1000>',
-        plp: '240x240>',
-        zoomed: '1000x1000>'
-      }
-    end
-
-    def create_sizes
-      Spree::Image.styles.keys.each do |style|
-        obj = self.url(style)
-        obj.processed
-      end
-    end
-
-    def my_cf_image_url(style)
-      default_options = Rails.application.default_url_options
-      ActiveStorage::Current.host = default_options[:host]
-      str = self.url(style).url
-      path = str.split('//').last.split("/",2).last
-      Rails.env.development? ? str : "https://#{ENV['CLOUDFRONT_ASSET_URL']}/#{path}"
-    end
-
   end
+
+  def my_cf_image_url(style)
+    default_options = Rails.application.default_url_options
+    ActiveStorage::Current.host = default_options[:host]
+    str = self.url(style).url
+    path = str.split('//').last.split("/",2).last
+    Rails.env.development? ? str : "https://#{ENV['CLOUDFRONT_ASSET_URL']}/#{path}"
+  end
+
 end
-::Spree::Image.prepend(Spree::ImageDecorator)
