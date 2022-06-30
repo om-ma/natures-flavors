9a675a37bab78f849551c4cec09bdeddd1390ef7
Duc T. Lam <ducl_13@yahoo.com>
2022-06-30 14:25:57 -0700

    Duplicate file

--- app/migration/sql/update_meta_data.sql
+++
@@ -1,3 +0,0 @@
--- SQL to update meta data without "|" for doofinder
-update spree_products set meta_description = replace(meta_description, '|', ',');
-update spree_products set meta_keywords = replace(meta_keywords, '|', ',');
