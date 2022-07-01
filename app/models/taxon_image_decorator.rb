7e50846c34563bd554d97b66bb9e8244d307ecfb
Duc T. Lam <ducl_13@yahoo.com>
2022-07-01 16:41:50 -0700

    Fix category image from CDN issue

--- app/models/taxon_image_decorator.rb
+++ app/models/taxon_image_decorator.rb
@@ -3,7 +3,7 @@ Spree::TaxonImage.class_eval do
   def my_cf_image_url()
     default_options = Rails.application.default_url_options
     ActiveStorage::Current.host = default_options[:host]
-    str = self.original_url
+    str = self.attachment.url
     path = str.split('//').last.split("/",2).last
     Rails.env.development? ? str : "https://#{ENV['CLOUDFRONT_ASSET_URL']}/#{path}"
   end
