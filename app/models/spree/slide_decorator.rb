c53a9300c197c2611181df08281b9e6a144d7fbe
Duc T. Lam <ducl_13@yahoo.com>
2022-06-30 20:49:12 -0700

    Call url method

--- app/models/spree/slide_decorator.rb
+++ app/models/spree/slide_decorator.rb
@@ -4,7 +4,7 @@ module Spree
   	def my_cf_mobile_image_url
 	    default_options = Rails.application.default_url_options
 	    ActiveStorage::Current.host = default_options[:host]
-	    str = self.slide_mobile_image.service_url
+	    str = self.slide_mobile_image.url
 	    path = str.split('//').last.split("/",2).last
 	    Rails.env.development? ? str : "https://#{ENV['CLOUDFRONT_ASSET_URL']}/#{path}"
 	  end
@@ -12,7 +12,7 @@ module Spree
 	  def my_cf_desktop_image_url
 	    default_options = Rails.application.default_url_options
 	    ActiveStorage::Current.host = default_options[:host]
-	    str = self.slide_image.service_url
+	    str = self.slide_image.url
 	    path = str.split('//').last.split("/",2).last
 	    Rails.env.development? ? str : "https://#{ENV['CLOUDFRONT_ASSET_URL']}/#{path}"
 	  end
