bc485ec16620df75cf94c5ef489c2efea9708edc
Duc T. Lam <ducl_13@yahoo.com>
2022-07-20 11:00:39 -0700

    Change association from users to taxons_users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
 	def set_categories
 		@all_categories = Rails.cache.fetch("@all_categories", expires_in: Rails.configuration.x.cache.expiration) do
-			@product_category	= Spree::Taxon.includes(children: :users).references(children: :users).find_by_name("PRODUCTS")
+			@product_category	= Spree::Taxon.includes(children: :taxons_users).references(children: :taxons_users).find_by_name("PRODUCTS")
 			(@product_category.present? ? @product_category&.children.order(:position): [])
 		end
 	end
