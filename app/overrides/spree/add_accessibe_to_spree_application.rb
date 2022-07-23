8e2a1cc94c950783252c754dae9f306203196704
Duc T. Lam <ducl_13@yahoo.com>
2022-07-22 21:25:16 -0700

    Move accessibe to head for turbolinks

--- app/overrides/spree/add_accessibe_to_spree_application.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/layouts/spree_application',
-  name: 'add_accessibe_to_spree_application',
-  insert_bottom: "[data-hook='body']",
-  partial: 'spree/shared/accessibe'
-)
