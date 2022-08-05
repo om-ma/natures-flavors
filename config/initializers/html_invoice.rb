0d958b496fb18e4d5f6168b078f3b95faf91f317
Duc T. Lam <ducl_13@yahoo.com>
2022-08-05 11:56:20 -0700

    Conflict with default spree logo.png. Renaming

--- config/initializers/html_invoice.rb
+++ config/initializers/html_invoice.rb
@@ -1,3 +1,3 @@
 
-Spree::HtmlInvoice::Config.set(html_invoice_logo_path: "logo.png")
+Spree::HtmlInvoice::Config.set(html_invoice_logo_path: "logo-nf.png")
 Spree::HtmlInvoice::Config.set(print_buttons: "invoice,commercial_invoice,pick_list,packing_list")
