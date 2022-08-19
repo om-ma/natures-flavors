ed49d27661c1696ccac49b5c8e475a78cd54c3fb
Duc T. Lam <ducl_13@yahoo.com>
2022-08-18 21:47:29 -0700

    Category page for top level taxons

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -8,12 +8,29 @@ Spree::TaxonsController.class_eval do
     params['per_page'] = @per_page
 
     if !http_cache_enabled? || stale?(etag: etag, last_modified: last_modified, public: true)
-      load_products
+      @is_top_level_menu_item = @taxon.is_top_level_menu_item
+      if @is_top_level_menu_item
+        load_most_popular_products
+      else
+        load_products
+      end
     end
   end
 
   def load_taxon_with_children
     @taxon = Spree::Taxon.includes(children: :children).references(children: :children).friendly.find(params[:id])
+  end
+
+  def load_most_popular_products
+    search_params = {
+      sort_by: 'descend_by_popularity',
+      current_store: current_store,
+      taxon: @taxon,
+      include_images: true
+    }
+    
+    @searcher = build_searcher(search_params)
+    @products = products_searcher = @searcher.retrieve_products.includes(:product_properties, :prices, :sale_prices).references(:product_properties, :prices, :sale_prices).limit(6)
   end
 
   def load_products    
