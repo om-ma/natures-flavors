9ea4d102226786c8008f12497d128de4421edc92
Duc T. Lam <ducl_13@yahoo.com>
2022-08-21 02:45:21 -0700

    Move from www2 to www

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: "www2.naturesflavors.com" }
+  config.action_mailer.default_url_options = { host: "www.naturesflavors.com" }
   config.action_mailer.asset_host = "https://#{ENV['CLOUDFRONT_ASSET_URL']}"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -156,5 +156,5 @@ SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(aws_access_k
 SitemapGenerator::Sitemap.sitemaps_host = "http://naturesflavors-production-sitemaps.s3.amazonaws.com/"
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
-Rails.application.default_url_options = { host: "https://www2.naturesflavors.com" }
-Spree::Core::Engine.routes.default_url_options[:host] = 'www2.naturesflavors.com'
+Rails.application.default_url_options = { host: "https://www.naturesflavors.com" }
+Spree::Core::Engine.routes.default_url_options[:host] = 'www.naturesflavors.com'
