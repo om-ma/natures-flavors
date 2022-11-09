41393653f57a31a6760bbb8b018cfd601f926cc4
Duc T. Lam <ducl_13@yahoo.com>
2022-11-09 09:41:32 -0800

    Use db write connection

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -13,6 +13,9 @@ Spree::OrdersController.class_eval do
     @order = current_order || current_store.orders.incomplete.
              includes(line_items: [variant: [:images, :product, option_values: :option_type]]).
              find_or_initialize_by(token: cookies.signed[:token])
-    associate_user
+    
+    ActiveRecord::Base.connected_to(role: :writing) do
+      associate_user
+    end
   end
 end
