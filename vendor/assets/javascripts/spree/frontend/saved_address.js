4afbe19d816b0f1798e40ad916f41365fa6e7942
Duc T. Lam <ducl_13@yahoo.com>
2022-12-12 10:48:14 -0800

    Clean up

--- vendor/assets/javascripts/spree/frontend/saved_address.js
+++ vendor/assets/javascripts/spree/frontend/saved_address.js
@@ -2,7 +2,6 @@ $( document ).on('turbolinks:load', function() {
     $('.address-select-change-js').on('change', function() {
       if (this.value == "new_address_selected"){
         $("#order_bill_address_id_0").click();
-        //console.log(document.getElementById("billing"))
         $('#billing').find('input:text').val('');
       }
       else{
