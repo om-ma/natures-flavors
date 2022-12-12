e18894400b81b7248b4c0e0463720383c94ac4a8
Duc T. Lam <ducl_13@yahoo.com>
2022-12-12 10:45:10 -0800

    Clear other is_default addresses

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -3,6 +3,16 @@ module Spree
     def self.prepended(base)
       base.attr_accessor :is_address_save
       base.scope :defaults, -> {where(is_default: true) }
+      base.before_save :clear_other_is_default
+    end
+
+    def clear_other_is_default
+      if is_default && user.present?
+        Spree::Address.where.not(id: id).where(user_id: 1, is_default: true, deleted_at: nil).each do |address|
+          address.is_default = false
+          address.save!
+        end
+      end
     end
   end
 end
