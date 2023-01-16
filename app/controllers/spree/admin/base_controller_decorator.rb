b1ab6361351d197a4a17b375fb94ca3d0b3a4c99
Duc T. Lam <ducl_13@yahoo.com>
2023-01-15 20:27:00 -0800

    Use write databse connection

--- app/controllers/spree/admin/base_controller_decorator.rb
+++ app/controllers/spree/admin/base_controller_decorator.rb
@@ -1,4 +1,20 @@
 Spree::Admin::BaseController.class_eval do
   include Spree::CacheHelper
+  
+  def admin_oauth_token
+    user = try_spree_current_user
+    return unless user
+
+    ActiveRecord::Base.connected_to(role: :writing) do
+      @admin_oauth_token ||= begin
+        Doorkeeper::AccessToken.active_for(user).where(application_id: admin_oauth_application.id).last ||
+          Doorkeeper::AccessToken.create!(
+            resource_owner_id: user.id,
+            application_id: admin_oauth_application.id,
+            scopes: admin_oauth_application.scopes
+          )
+      end.token
+    end
+  end
 
 end
