4765f9d4651d13dd58079cb4b3f2fbde91b0e7dd
Duc T. Lam <ducl_13@yahoo.com>
2023-04-12 14:36:53 -0700

    Check if url is a string already

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -17,7 +17,14 @@ Spree::Image.class_eval do
   def my_cf_image_url(style)
     default_options = Rails.application.default_url_options
     ActiveStorage::Current.host = default_options[:host]
-    str = self.url(style).url
+    
+    my_url = self.url(style)
+    if (my_url.is_a? String)
+      str = my_url
+    else
+      str = my_url.url
+    end
+
     path = str.split('//').last.split("/",2).last
     Rails.env.development? ? str : "https://#{ENV['CLOUDFRONT_ASSET_URL']}/#{path}"
   end
